330bd67edec72305a2b7a766249183a4246b7f57
@aatifmd7218 <mohammad.aatif@oxcytech.com>
2024-10-10 13:12:14 +0530

    schedule api create

--- src/app/(blog)/allblogemployee/page.js
+++ src/app/(blog)/allblogemployee/page.js
@@ -1,6 +1,5 @@
 "use client";
 import AllBlogEmployeeTable from "@/components/employee/AllBlogEmployeeTable";
-import SideNav from "@/components/sidebar/SideNav";
 import { useSession } from "next-auth/react";
 
 export default function Page() {
@@ -14,18 +13,8 @@ export default function Page() {
     return <div>Access Denied</div>;
   }
   return (
-    <>
-      <div className=" px-6 py-10 sm:px-8 sm:py-16 ">
-        <div className="grid grid-cols-1 lg:grid-cols-12 gap-y-10 sm:gap-x-10">
-          <div className=" col-span-3 space-y-10">
-            <SideNav />
-          </div>
-
-          <div className="col-span-9">
-            <AllBlogEmployeeTable />
-          </div>
-        </div>
-      </div>
-    </>
+    <div>
+      <AllBlogEmployeeTable />
+    </div>
   );
 }
